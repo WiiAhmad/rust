d540d279a252494a76ef085b3c62a1f709539d2d
Ahmad maulana yusuf <77580091+WiiAhmad@users.noreply.github.com>
2024-11-26 16:20:11 +0700

    feat: test sampai operasi boolean

--- learn/src/main.rs
+++ learn/src/main.rs
@@ -1,3 +1,60 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[test]
+fn test(){
+    println!("test func test")
+}
+
+#[test]
+fn test_variable() {
+    let name = "ahmad";
+
+    println!("hello {}", name)
+    //one line comment
+    /*
+    
+    two or more line comment */
+}
+
+#[test]
+fn test_type() {
+    let int: i32 =  10;
+
+    println!("int : {}", int)
+}
+
+#[test]
+fn test_operations() {
+    let a =  10;
+    let b = 20;
+    
+    let c = a + b;
+    let d = a - b;
+    let e =  a * b;
+    let f = a / b;
+
+    println!("{} {} {} {}", c,d,e,f)
+}
+
+#[test]
+fn augmented_assigment() {
+    let mut a = 10;
+    println!("{}", a);
+
+    a += 10;
+    println!("{}", a);
+
+    a -= 20;
+    println!("{}", a);
+}
+
+#[test]
+fn boolean() {
+    let a =  true;
+    let b: bool = false;
+
+    println!("boolean :{} :{}", a , b)
+}
